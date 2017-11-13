d56f4a0b89d494d745189ee0e80afa895d6601ad
SHINYA MIYATA <kamiefan@gmail.com>
2017-11-14 00:10:58 +0900

    Vueのテスト

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,4 +8,11 @@
 
 @section('content')
     <p>You are logged in!</p>
+    <div id="app">
+        <example-component></example-component>
+    </div>
 @stop
+
+@push('js')
+    <script src="{{ mix('js/app.js') }}"></script>
+@endpush
