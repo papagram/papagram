65ad569d136d6be9f7a872c931b08ca748d735bd
SHINYA MIYATA <kamiefan@gmail.com>
2017-11-13 22:29:25 +0900

    laravel-snappyを再導入

--- resources/views/admin/slip/cards/pdf.blade.php
+++ resources/views/admin/slip/cards/pdf.blade.php
@@ -1,1 +1,18 @@
-<p style="background: red">hello world</p>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <link href="https://fonts.googleapis.com/earlyaccess/notosansjp.css" rel="stylesheet">
+        <style>
+        .page-break {
+            page-break-after: always;
+        }
+        </style>
+    </head>
+    <body>
+        <h1>Page 1</h1>
+        <p style="background: red;font-family: 'Noto Sans JP';">ハローワールド</p>
+        <div class="page-break"></div>
+        <h1>Page 2</h1>
+    </body>
+</html>
