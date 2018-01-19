2e59c34c7064fb78199199430cd24db0b2118a73
SHINYA MIYATA <kamiefan@gmail.com>
2018-01-19 15:12:18 +0900

    マイグレーションファイルを修正

--- database/migrations/2018_01_18_222550_create_clients_table.php
+++ database/migrations/2018_01_18_222550_create_clients_table.php
@@ -17,15 +17,15 @@ class CreateClientsTable extends Migration
 	{
 		Schema::create('clients', function(Blueprint $table) {
             $table->increments('id');
-            $table->string('code', 10)->nullable()->comment('管理コード');
+            $table->string('code', 10)->unique()->comment('管理コード');
             $table->string('name');
-            $table->string('tel1', 3);
-            $table->string('tel2', 4);
-            $table->string('tel3', 4);
-            $table->string('postcode1', 3);
-            $table->string('postcode2', 4);
-            $table->string('address');
-            $table->string('email')->unique();
+            $table->string('tel1', 3)->nullable();
+            $table->string('tel2', 4)->nullable();
+            $table->string('tel3', 4)->nullable();
+            $table->string('postcode1', 3)->nullable();
+            $table->string('postcode2', 4)->nullable();
+            $table->string('address')->nullable();
+            $table->string('email')->unique()->nullable();
             $table->string('url')->nullable();
             $table->timestamps();
 		});
