b1319650069bc28151a998f26ff22dac90527348
SHINYA MIYATA <kamiefan@gmail.com>
2018-02-03 16:42:00 +0900

    見積情報の項目をフォームに追加

--- resources/views/admin/estimates/info_fields.blade.php
+++ resources/views/admin/estimates/info_fields.blade.php
@@ -8,3 +8,28 @@
         'data-mindate' => 'today',
     ]
 ) !!}
+
+{!! Form::bsText(
+    'client_name',
+    $estimate->client_name,
+    'estimate.client_name'
+) !!}
+
+{!! Form::bsText(
+    'subject',
+    $estimate->subject,
+    'estimate.subject'
+) !!}
+
+{!! Form::bsText(
+    'expiration_date',
+    $estimate->expiration_date,
+    'estimate.expiration_date',
+    [
+        'class' => 'form-control datepicker',
+        'placeholder' => '見積有効期限を選択',
+        'data-mindate' => 'today',
+    ]
+) !!}
+
+{!! Form::bsTextarea('note', $estimate->note, 'estimate.note') !!}
