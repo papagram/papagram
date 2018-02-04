f0085910038bf8f5244e452e03d30005fe46e35e
SHINYA MIYATA <kamiefan@gmail.com>
2018-02-04 17:06:07 +0900

    マイグレーションを修正　ポリモーフィックリレーションを追加

--- database/migrations/2018_02_03_162730_create_items_table.php
+++ database/migrations/2018_02_03_162730_create_items_table.php
@@ -21,6 +21,7 @@ class CreateItemsTable extends Migration
             $table->integer('number')->comment('数量');
             $table->integer('unit_price')->comment('単価');
             $table->integer('subtotal')->comment('小計');
+            $table->morphs('itemable');
             $table->timestamps();
 		});
 	}
