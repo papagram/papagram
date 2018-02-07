972cf5765486b0f7b3a4df48140e06d18242b604
SHINYA MIYATA <kamiefan@gmail.com>
2018-02-08 00:16:25 +0900

    コロン追加

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -8,7 +8,7 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
-Vue.prototype.$http = axios
+Vue.prototype.$http = axios;
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
