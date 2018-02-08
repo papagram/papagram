046338e8b837863608bf6fb9a62b248bbc3442d8
SHINYA MIYATA <kamiefan@gmail.com>
2018-02-09 00:04:35 +0900

    日本語化

--- resources/lang/ja/estimate.php
+++ resources/lang/ja/estimate.php
@@ -2,4 +2,12 @@
 
 return [
     'model_name' => '見積書',
+    'issue_date' => '発行日',
+    'client_name' => '見積先名称',
+    'subject' => '件名',
+    'expiration_date' => '有効期限',
+    'note' => '備考',
+    'subtotal' => '小計',
+    'consumption_tax' => '消費税',
+    'amount_total' => '合計金額',
 ];
---
+++ resources/lang/ja/item.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'name' => '項目名',
+    'number' => '数量',
+    'unit_price' => '単価',
+    'line_price' => '金額',
+];
